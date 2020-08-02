ef4cc758012397fc58c5defb7a7cd18c01db860b
prashantSunwar <budhisunwar20@gmail.com>
2020-08-03 01:05:28 +0530

    added router

---
+++ src/router/Details.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function snakeCase(word) {
+  return word.replace(/([A-Z])/g, " $1").replace(/^./, function (str) {
+    return str.toUpperCase();
+  });
+}
+
+function Details({ location }) {
+  const countryInfo = Object.entries(location.state.info);
+  countryInfo.splice(5, 1);
+  return (
+    <main className="details">
+      <Link to="/" className="back-route">
+        <FontAwesomeIcon icon="arrow-left" />
+        Back
+      </Link>
+      <section className="country">
+        <div className="country-grid">
+          <img src={location.state.info.flag} alt={location.name} />
+          <div className="country-cont">
+            <h1>{location.state.info.name} </h1>
+            <div className="country-info">
+              {countryInfo.map((country) => (
+                <p className="flag__subtext" key={country[0]}>
+                  {snakeCase(country[0])}:&nbsp;
+                  <span className="flag__subtext-info">
+                    {typeof country[1] === "number" ? country[1].toLocaleString() : country[1]}
+                  </span>
+                </p>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default Details;
