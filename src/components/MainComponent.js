3841c3f6ab57258a545eba9f65d4fbd1fe705ab6
prashantSunwar <budhisunwar20@gmail.com>
2020-08-02 19:02:19 +0530

    added response array in state

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchPanel from "./SearchPanel";
 import ResultPanel from "./ResultPanel";
 
 function MainComponent() {
+  const [countries, setCountries] = useState([]);
+
   return (
     <main className="main">
       <div className="container">
-        <SearchPanel />
-        <ResultPanel />
+        <SearchPanel setCountries={setCountries} />
+        <ResultPanel countries={countries} />
       </div>
     </main>
   );
